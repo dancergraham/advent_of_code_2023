e2813290d150c209d3e9852057599205bc185e65
dancergraham <graham.knapp@gmail.com>
2023-12-06 09:09:27 +0100

    feature: get the day automatically

--- src/add.py
+++ src/add.py
@@ -1,3 +1,4 @@
+import datetime
 import shutil
 import sys
 
@@ -14,7 +15,14 @@ def try_get_input(day: int):
 
 
 def main():
-    day = sys.argv[1].zfill(2)
+    try:
+        day = sys.argv[1].zfill(2)
+    except IndexError:
+        date = datetime.date.today()
+        if date <= datetime.date(2023, 12, 25):
+            day = str(date.day).zfill(2)
+        else:
+            print("You need to say what day to add")
     print("Creating new day")
     print("Did you remember to change branch?")
     print(shutil.copytree("./advent_of_code_2023/_template/", f"./advent_of_code_2023/day{day}/"))
